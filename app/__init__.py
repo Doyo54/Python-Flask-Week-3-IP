db0c58ab9a880a7570c4105d744372b618230795
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-10 11:30:05 +0300

    ft: Block content created in index.html file

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
-from ensurepip import bootstrap
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from ..config import DevConfig
+from config import DevConfig
 
 # Initializing application
 app = Flask(__name__)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+
 
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
