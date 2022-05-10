0f0e4031186947a030e49bddf9502bb5afd62d4b
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-10 22:37:25 +0300

    ft: Database created and class to save user information created

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ class Config:
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://doyo:doyo123@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 
--- manage.py
+++ manage.py
@@ -1,4 +1,12 @@
-from app import app
+from app import app,db
+from app.models import User 
+from flask_script import Manager,Server
 
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 if __name__ == '__main__':
-    app.run()
+    manager.run()
