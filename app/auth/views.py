d4177cfd96ab9d383b130760c3e35acac20f7d8c
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-10 21:17:06 +0300

    ft: Register function created

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,3 +8,11 @@ def login():
 
     # flash('Invalid username or Password')
     return render_template('auth/login.html', login_form = login_form)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
